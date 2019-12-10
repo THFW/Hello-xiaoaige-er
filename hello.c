aa50637aaffdde9b9a3976ce279b5615ad3379f5
Yang Chen Long <820371826@qq.com>
2019-12-10 12:44:37 +0800

    add  hello.c

--- hello.c
+++ hello.c
@@ -1,36 +1,7 @@
 #include<stdio.h>
-#include <string.h>
-#define M 100
 int main()
 {
-    int a=0,b=0,c=0,d=0,e=0,n=0;
-	char aa[M];
-	char * bb="";
-	b=0,c=0,d=0,e=0;
-	gets(aa);
-	for(a=0;a<M;a++)
-	{
-		if(aa[a]=='\0')
-		break;	
-		else
-		n++;
-	}
-	for(a=0;a<n;a++)
-	{
-		if(((aa[a]>='A'&&aa[a]<='Z')||(aa[a]>='a'&&aa[a]<='z')))
-			b++;
-		else if(aa[a]==' ')
-			c++;
-		else if(aa[a]>='0'&&aa[a]<='9')
-			d++;
-		else
-			e++;
-	}
-
-	if(strcmp(aa,bb)!=0)
-	{
-		printf("%d\n%d\n%d\n%d",b,c,d,e);
-	}
-
+    int a,b;
+    while(~scanf("%d%d", &a, &b))printf("%d\n",a+b);
     return 0;
 }
